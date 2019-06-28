bc6a5e8d1ef8720b2f25ab2c9c12ab25571cf531
toddnguyen47 <todd.nguyen47@gmail.com>
2019-06-28 00:36:53 -0700

    Initial commit

---
+++ src/main/b_view/ListUsersView.java
@@ -0,0 +1,16 @@
+package main.b_view;
+
+
+/**
+ * The view that lists all the users
+ * @author ToddNguyen
+ *
+ */
+public class ListUsersView extends BaseView {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+}
